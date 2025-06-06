8da5b4b2ad2f8350478d52626f5e7383419abccb
Melih Berat ŞANLI <mberatsanli@gmail.com>
2025-06-06 20:32:02 +0200

    right and left buttons

--- include/button_handler.h
+++ include/button_handler.h
@@ -6,6 +6,8 @@
 #define BUTTON_UP 12     // D12
 #define BUTTON_DOWN 13   // D13
 #define BUTTON_ACTION 14 // D14
+#define BUTTON_LEFT 27   // D27
+#define BUTTON_RIGHT 26  // D26
 
 class Button {
   private:
@@ -23,6 +25,8 @@ class Buttons {
   public:
     Button up;
     Button down;
+    Button left;
+    Button right;
     Button action;
     Buttons();
 };
--- src/button_handler.cpp
+++ src/button_handler.cpp
@@ -7,9 +7,9 @@ Button::Button(uint8_t buttonPin) : pin(buttonPin), pressed(false), lastPressTim
 bool Button::isPressed() {
     unsigned long currentTime = millis();
     int buttonState = digitalRead(pin);
-    
-    if (buttonState == LOW) {
-        if (!pressed && (currentTime - lastPressTime > DEBOUNCE_TIME)) {
+
+    if(buttonState == LOW) {
+        if(!pressed && (currentTime - lastPressTime > DEBOUNCE_TIME)) {
             pressed = true;
             lastPressTime = currentTime;
             return true;
@@ -21,6 +21,6 @@ bool Button::isPressed() {
     return false;
 }
 
-Buttons::Buttons() : up(BUTTON_UP), down(BUTTON_DOWN), action(BUTTON_ACTION) {}
+Buttons::Buttons() : up(BUTTON_UP), down(BUTTON_DOWN), left(BUTTON_LEFT), right(BUTTON_RIGHT), action(BUTTON_ACTION) {}
 
 Buttons buttons;
